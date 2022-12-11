1ef018eb7e7752b3c07a29349e3d9548ca6f9e9d
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2022-12-11 01:48:07 -0500

    setup terraform to deploy job

--- main.tf
+++ main.tf
@@ -1,36 +1,105 @@
-# Configure the Google provider
-provider "google" {
-  credentials = file("google.json")
-  project     = "<project-id>"
-  region      = "us-central1"
+variable "project_id" {
+  type = string
 }
 
-# Create a Cloud Run service
-resource "google_run_service" "docs-sync" {
-  name       = "docs-sync"
-  location   = "us-central1"
-  platform   = "managed"
-  metadata = {
-    namespace = "<namespace>"
-  }
+variable "location" {
+  type = string
+}
 
-  template {
-    spec {
-      container {
-        image = "docs-sync"
-      }
+variable "access_token" {
+  type = string
+}
+
+variable "billing_account" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.45.0"
     }
   }
 }
 
+# Configure the Google provider
+provider "google" {
+  project      = var.project_id
+  region       = var.location
+  access_token = var.access_token
+}
+
+# Create a project
+resource "google_project" "doc_sync_project" {
+  name            = "Doc Sync Project"
+  project_id      = "doc-sync"
+  billing_account = var.billing_account
+}
+
+# Create service account
+resource "google_service_account" "docs_sync_service_account" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+# Enable cloud run service
+resource "google_project_service" "cloud_run" {
+  project                    = var.project_id
+  service                    = "run.googleapis.com"
+  disable_dependent_services = true
+  depends_on = [
+    google_project.doc_sync_project,
+    google_service_account.docs_sync_service_account
+  ]
+}
+
+# Create a Cloud Run service
+resource "google_cloud_run_service" "docs_sync_service" {
+  name     = "docs-sync-service"
+  location = var.location
+
+  template {
+    spec {
+      containers {
+        image = "ghcr.io/lobarr/docs-sync"
+      }
+      service_account_name = google_service_account.docs_sync_service_account.email
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [google_project_service.cloud_run]
+}
+
 # Create a Cloud Scheduler job to run the Cloud Run service
-resource "google_scheduler_job" "docs-sync" {
-  name         = "docs-sync-job"
-  description  = "Run the docs-sync Cloud Run service every week"
-  schedule     = "0 0 * * 0" # At 00:00 on Sunday
-  time_zone    = "America/New_York"
+resource "google_cloud_scheduler_job" "docs_sync_job" {
+  name        = "docs-sync-job"
+  description = "Run the docs-sync Cloud Run service every week"
+  schedule    = "0 0 * * 0" # At 00:00 on Sunday
+  time_zone   = "America/New_York"
   http_target {
     http_method = "POST"
-    uri         = "${google_run_service.docs-sync.status[0].url}"
+    uri         = google_cloud_run_service.docs_sync_service.status[0].url
   }
+  depends_on = [
+    google_cloud_run_service.docs_sync_service
+  ]
+}
+
+# Display useful context from dpeloyments 
+output "project_number" {
+  value = google_project.doc_sync_project.number
+}
+
+output "serivce_account" {
+  value = google_service_account.docs_sync_service_account.id
+}
+
+output "cloud_run_service_url" {
+  value = google_cloud_run_service.docs_sync_service.status[0].url
 }
--- makefile
+++ makefile
@@ -2,3 +2,8 @@ IMAGE_NAME="docs-sync"
 
 build-image:
 	docker build --compress -t $IMAGE_NAME:latest .
+
+terraform:
+	terraform init
+	terraform validate
+	terraform plan
