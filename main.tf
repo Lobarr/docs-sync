a5790409ccd5d925cd9d1ed4984cc6ff42220ad0
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2022-12-11 11:38:48 -0500

    clean up tf setup and connect to gcloud

--- main.tf
+++ main.tf
@@ -6,11 +6,7 @@ variable "location" {
   type = string
 }
 
-variable "access_token" {
-  type = string
-}
-
-variable "billing_account" {
+variable "service_account" {
   type = string
 }
 
@@ -25,21 +21,8 @@ terraform {
 
 # Configure the Google provider
 provider "google" {
-  project      = var.project_id
-  region       = var.location
-  access_token = var.access_token
-}
-# Create a project
-resource "google_project" "doc_sync_project" {
-  name            = "Doc Sync Project"
-  project_id      = "doc-sync"
-  billing_account = var.billing_account
-}
-
-# Create service account
-resource "google_service_account" "docs_sync_service_account" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
+  project = var.project_id
+  region  = var.location
 }
 
 # Enable cloud run service
@@ -47,10 +30,13 @@ resource "google_project_service" "cloud_run" {
   project                    = var.project_id
   service                    = "run.googleapis.com"
   disable_dependent_services = true
-  depends_on = [
-    google_project.doc_sync_project,
-    google_service_account.docs_sync_service_account
-  ]
+}
+
+# Enable cloud schduler service
+resource "google_project_service" "cloud_scheduler" {
+  project                    = var.project_id
+  service                    = "cloudscheduler.googleapis.com"
+  disable_dependent_services = true
 }
 
 # Create a Cloud Run service
@@ -63,7 +49,7 @@ resource "google_cloud_run_service" "docs_sync_service" {
       containers {
         image = "ghcr.io/lobarr/docs-sync"
       }
-      service_account_name = google_service_account.docs_sync_service_account.email
+      service_account_name = var.service_account
     }
   }
 
@@ -91,14 +77,6 @@ resource "google_cloud_scheduler_job" "docs_sync_job" {
 }
 
 # Display useful context from dpeloyments 
-output "project_number" {
-  value = google_project.doc_sync_project.number
-}
-
-output "serivce_account" {
-  value = google_service_account.docs_sync_service_account.id
-}
-
 output "cloud_run_service_url" {
   value = google_cloud_run_service.docs_sync_service.status[0].url
 }
