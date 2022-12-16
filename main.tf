5bdac063bf7f7bd624ba6bd12b763ce7ff36184a
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2022-12-16 01:26:59 -0500

    update pipenv cache key and enable serviceusage.googleapis.com

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ provider "google" {
 # Enable required cloud services
 resource "google_project_service" "services" {
   for_each = toset([
+    "serviceusage.googleapis.com",
     "run.googleapis.com",
     "cloudscheduler.googleapis.com",
     "containerregistry.googleapis.com",
